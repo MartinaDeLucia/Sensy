12812a7f265eb924010f71c9644f571ed43f213e
Gregorio Garofalo <garofalogregorio@gmail.com>
2024-12-27 18:41:56 +0100

    Aggiunta funzione di riduzione dimensionalità e codifica delle domande negli embeddings di BERT

--- clustering/question_clustering.py
+++ clustering/question_clustering.py
@@ -1,5 +1,15 @@
 import json
 
+from sentence_transformers import SentenceTransformer
+from sklearn.decomposition import PCA
+
+
+# Funzione per codificare le domande negli embeddings di Sentence-BERT
+def encode_questions(questions, model_name='all-MiniLM-L6-v2'):
+    """Convert text questions into semantic embeddings using Sentence-BERT."""
+    model = SentenceTransformer(model_name)
+    return model.encode(questions)
+
 # Funzione per caricare le domande dal file json
 def load_questions_from_json(file_path):
     """Load questions and sensitivity labels from a JSON file."""
@@ -8,6 +18,11 @@ def load_questions_from_json(file_path):
     sensitive = [item["question_en"] for item in data if item["sensitive?"] == 1]
     non_sensitive = [item["question_en"] for item in data if item["sensitive?"] == 0]
     return sensitive, non_sensitive
+
+# Funzione per ridurre la dimensionalità tramite PCA
+def reduce_dimensions(embeddings, n_components=50):
+    pca = PCA(n_components=n_components, random_state=42)
+    return pca.fit_transform(embeddings)
 
 
 if __name__ == "__main__":
@@ -29,3 +44,8 @@ if __name__ == "__main__":
     print("Caricamento delle domande dal JSON...")
 
     sensitive_questions, non_sensitive_questions = load_questions_from_json(file_path)
+
+    # Step 2 : Elaborazione delle domande sensitive
+
+    sensitive_embeddings = encode_questions(sensitive_questions)
+    reduced_sensitive_embeddings = reduce_dimensions(sensitive_embeddings)
