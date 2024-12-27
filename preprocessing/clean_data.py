c40e5ba88743f637cda83c4bf5be80c4546e820e
ALESSANDRA RAIA <a.raia7@studenti.unisa.it>
2024-12-27 18:40:42 +0100

    pre-processing: clean_data.py che pulisce il dataset (JSON) converito in dataframe, rimuovendo i duplicati e tokenizzando il testo; In feature_extraction.py ho definito il dizionario delle parole sensibili.

---
+++ preprocessing/clean_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import nltk
+import json
+
+def clean_dataset(file_path):
+    """
+    Carica e pulisce il dataset da un file JSON.
+    """
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # Converti il JSON in DataFrame
+    df = pd.DataFrame(data)
+    print(f"Prima della pulizia di {file_path} sono: {df.count()}")
+
+    # Rimuovi duplicati
+    df = df.drop_duplicates()
+    print(f"Dopo la pulizia di {file_path} sono: {df.count()}")
+
+    # Se esiste la colonna 'question', eliminala
+    if 'question' in df.columns:
+        df = df.drop(columns=['question'])
+
+    # Tokenizzazione del testo in inglese
+    df["question_tokenized"] = df["question_en"].apply(nltk.word_tokenize)
+
+    return df
